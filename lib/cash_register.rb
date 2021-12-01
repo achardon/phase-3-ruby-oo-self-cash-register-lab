f56f35074853e5de1163807a74d22a90257ec682
Alessandra Chardon <achardon@ucla.edu>
2021-12-01 10:45:21 -0500

    commit tree 045a9493bcecd79843ca5ea482d552c688b153d8

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,34 +2,48 @@ require "pry"
 
 class CashRegister
 
-    attr_accessor :total, :discount
+    attr_accessor :total, :discount, :items_array, :last_transaction_item, :last_transaction_price
 
     def initialize(discount = 0)
-        @total = 0
+        @total = 0.00
+        @items_array = []
+        @last_transaction_item = ''
+        @last_transaction_price = 0
         @discount = discount
     end
 
     def add_item(title, price, quantity = 1)
         self.total += quantity * price
-        binding.pry
-        
+        quantity.times do
+            self.items_array.append(title)
+        end
+        self.last_transaction_item = title
+        self.last_transaction_price = quantity * price
     end
 
     def apply_discount
-        case self.discount
-        when self.discount != 0
-            self.total -= self.discount / 100 * total 
-            "After the discount, the total comes to $#{self.total}."
-        when self.discount == 0
+        float_discount = self.discount.to_f
+        if float_discount != 0
+            self.total -= float_discount / 100 * self.total 
+            "After the discount, the total comes to $#{self.total.to_i}."
+        elsif float_discount == 0
             "There is no discount to apply."
         end
     end
 
-    def items(one, two, three = nil)
-        [one, two, three]
+    def items
+        self.items_array
+    end
+
+    def void_last_transaction
+        self.items_array.pop
+        self.total -= self.last_transaction_price
     end
 
 end
 
 x = CashRegister.new
 x.add_item('eggs', 5.00)
+x.add_item('milk', 3.00)
+x.apply_discount
+x.void_last_transaction
